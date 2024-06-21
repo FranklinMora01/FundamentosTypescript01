a1ae91b0b462d521dea9dcd6f8d67fb932afe223
Franklin Mora <105868794+FranklinMora01@users.noreply.github.com>
2024-06-21 18:06:32 -0400

    ejemplo del metodo map de una arreglo

---
+++ src/metodos-array/2-map.ts
@@ -0,0 +1,7 @@
+//* el metodo map(), crea un nuevo array, el array resultante tiene una modificacion por cada elemento del array devuelto 
+
+const numeros = [10, 20, 30, 50];
+
+const resultadoMap = numeros.map( ElNumber => ElNumber * 2);
+
+console.log(resultadoMap);
---
+++ src/metodos-array/map.js
@@ -0,0 +1,5 @@
+"use strict";
+//* el metodo map(), crea un nuevo array, el array resultante tiene una modificacion por cada elemento del array devuelto 
+const numeros = [10, 20, 30, 50];
+const resultadoMap = numeros.map((ElNumber) => ElNumber * 2);
+console.log(resultadoMap);
